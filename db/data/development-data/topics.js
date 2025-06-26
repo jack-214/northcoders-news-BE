f7a6ff745e8ed3dc4015e4dc063763f242a3473f
Jack Ho <jack.ho12345@gmail.com>
2025-06-26 16:56:32 +0100

    changed topic coding image

--- db/data/development-data/topics.js
+++ db/data/development-data/topics.js
@@ -3,7 +3,7 @@ module.exports = [
     description: "Code is love, code is life",
     slug: "coding",
     img_url:
-      "https://plus.unsplash.com/premium_photo-1661877737564-3dfd7282efcb?q=80&w=2700&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://images.unsplash.com/photo-1515879218367-8466d910aaa4?q=80&w=1738&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     description: "FOOTIE!",
